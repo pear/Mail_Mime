1f6b20e9f5f40fcbff95764175c3cfd1cc5cdf9c
Aleksander Machniak <alec@php.net>
2009-12-21 11:47:18 +0000

    - added me in @author
    git-svn-id: http://svn.php.net/repository/pear/packages/Mail_Mime/trunk@292409 c90b9560-bf6c-de11-be94-00142212c4b1

--- mimePart.php
+++ mimePart.php
@@ -45,6 +45,7 @@
  * @author     Richard Heyes  <richard@phpguru.org>
  * @author     Cipriano Groenendal <cipri@php.net>
  * @author     Sean Coates <sean@php.net>
+ * @author     Aleksander Machniak <alec@php.net>
  * @copyright  2003-2006 PEAR <pear-group@php.net>
  * @license    http://www.opensource.org/licenses/bsd-license.php BSD License
  * @version    CVS: $Id$
