daf72e670996bb55ba481566ff42d5fc244096d8
Christian Weiske <cweiske@cweiske.de>
2015-08-21 07:44:04 +0200

    examples for GPG signing/encrypting

---
+++ examples/gpg-sign-encrypt.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Sign AND encrypt an email with PGP/gnupg
+ */
+require_once 'Mail.php';
+require_once 'Mail/mime.php' ;
+require_once 'Crypt/GPG.php';
+
+$mime = new Mail_mime(array('eol' => "\n"));
+$hdrs = array(
+    'From' => 'foo@example.org',
+    'Subject' => 'An encrypted mail example'
+);
+$mime->setTXTBody(
+    'This text and the attachment will be signed by foo@example.org'
+    . ' and only readable by bar@example.org'
+);
+$mime->addAttachment('/path/to/file', 'text/plain');
+
+$gpg = new Crypt_GPG();
+$gpg->addEncryptKey('bar@example.org');
+$gpg->addSignKey('foo@example.org', 'gpgpassword');
+$mime->setGPG($gpg);
+
+$body = $mime->get();
+$hdrs = $mime->headers($hdrs);
+
+$mail = Mail::factory('mail');
+//$mail->send('bar@example.org', $hdrs, $body);
+?>
---
+++ examples/gpg-sign.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Sign an email with PGP/gnupg
+ */
+require_once 'Mail.php';
+require_once 'Mail/mime.php' ;
+require_once 'Crypt/GPG.php';
+
+$mime = new Mail_mime(array('eol' => "\n"));
+$hdrs = array(
+    'From' => 'foo@example.org',
+    'Subject' => 'An encrypted mail example'
+);
+$mime->setTXTBody('This text will be signed by foo@example.org');
+
+$gpg = new Crypt_GPG();
+$gpg->addSignKey('foo@example.org', 'gpgpassword');
+$mime->setGPG($gpg);
+
+$body = $mime->get();
+$hdrs = $mime->headers($hdrs);
+
+$mail = Mail::factory('mail');
+//$mail->send('bar@example.org', $hdrs, $body);
+?>
